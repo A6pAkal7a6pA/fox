9283ad34ed182d553a42a3fd1ea060fa1c432abb
Festina Lente <mxm.klshv@gmail.com>
2021-04-06 21:16:14 +0300

    website done and ready to check

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.getElementById("popup-mint-link")?.addEventListener("click", function() {
+document.getElementById("popup-mint-link")?.addEventListener("click", function () {
 	document.querySelector(".mint-popup").classList.toggle("active");
 });
 document.querySelector(".mint-popup__exit")?.addEventListener("click", function () {
@@ -9,3 +9,38 @@ document.querySelector(".header__burger")?.addEventListener("click", function ()
 	document.querySelector(".header__menu").classList.toggle("active");
 });
 
+let array = document.querySelectorAll(".asset-content__title");
+array.forEach(el => {
+	el.addEventListener("click", function (event) {
+		if (event.target.className === "asset-content__title") {
+			event.target.parentElement.parentElement.classList.toggle("active");
+		} else if (event.target.className === "asset-content__dropdown") {
+			event.target.parentElement.parentElement.parentElement.classList.toggle("active");
+		}
+	})
+});
+
+let locations = window.location;
+let thisLocations = this.location;
+
+let menu = document.querySelectorAll(".menu__list-item");
+menu.forEach(el => {
+	el.childNodes.forEach(el => {
+	});
+});
+
+document.querySelector(".asset-info__pub-message")?.addEventListener("click", function () {
+	document.querySelector(".gallery-dop-popup-sww").classList.toggle("active");
+});
+
+document.querySelector(".gallery-dop-popup-sww__exit-link")?.addEventListener("click", function () {
+	document.querySelector(".gallery-dop-popup-sww").classList.remove("active");
+});
+
+document.querySelector(".asset-info__pub-message")?.addEventListener("contextmenu", function () {
+	document.querySelector(".gallery-dop-popup-tt").classList.toggle("active");
+});
+
+document.querySelector(".gallery-dop-popup-tt__exit-link")?.addEventListener("click", function () {
+	document.querySelector(".gallery-dop-popup-tt").classList.remove("active");
+});
